6239ef5661227523c0d8b4d2bc27f1ee35e89af8
New-two <nicealun1@sina.com>
2017-09-05 11:32:01 +0800

    v1.0.0 * 自定义链接地址

--- components/Footer/js/Footer.js
+++ components/Footer/js/Footer.js
@@ -2,11 +2,15 @@ import '../css/footer.css';
 import langPackage from '../js/lang'
 
 export default class Footer {
-    constructor(data='',options) {
+    constructor(data) {
         this.lang = data.lang || 'en';
         this.data = data;
-        this.langContent = langPackage[this.lang];
         this.from = data.from || '';
+        this.options = data.option;
+
+    }
+    _renderHtml() {
+        this.langContent = langPackage[this.lang];
         this.content = `<div id="foot" class="clearfix">
                             <div class="f-logo fl">
                                 <a href="//all.oasgames.com" rel="nofollow" target="_blank">
@@ -20,43 +24,18 @@ export default class Footer {
                                 <a target="_blank" href="//company.oasgames.com/en/" rel="nofollow">About US</a>
                                 丨<a target="_blank" href="/PrivacyPolicy(EN).html">Privacy Policy </a>
                                 丨<a target="_blank" href="/TermsofService(EN).html">Terms of Service</a>
-                        
                             </div>
                                 <p>©2012-2017 OASIS GAMES LIMITED. All rights reserved.</p>
                                 <p>All trademarks referenced herein are the properties of their respective owners.</p>
                             </div>
                         </div>`;
-        this.swc = `<div class="switch">
-                        <div class="current" id="footer_current" style="display: block;">
-                            English <img src="https://img.oasgames.com/upload/1503995547.png">
-                        </div>
-                        <div class="list" id="footer_list" style="display: none;">
-                            <div class="language" data-url="/zh">
-                                简体中文（ zh ）
-                            </div>
-                            <div class="language">
-                                English <img src="https://img.oasgames.com/upload/1503995535.png">
-                            </div>
-                        </div>
-                    </div>`;
-        this.hl = '';
-        this.options = options;
-
-    }
-
-    _renderHtml() {
-        if (this.options) {
-            this.hl = this.content + this.swc;
-        } else {
-            this.hl = this.content;
-        }
-        return this.hl;
+        return this.content;
     }
     fire(id) {
         let rootElement = document.getElementById(id);
         this.Footerwarp = id;
-        rootElement.innerHTML = this._renderHtml(this.data);
-        if(rootElement.children.length == 2){
+        rootElement.innerHTML = this._renderHtml();
+        if(this.options == 'ok'){
             rootElement.className = 'footer_box clearfix';
         }else{
             rootElement.className = '';
@@ -68,7 +47,6 @@ export default class Footer {
         this.alink = [];
         this.linkHml = [];
         let spikLink = document.querySelector('#'+this.Footerwarp+' .bottext');
-        console.log(spikLink);
         for(var item in json){
             this.alink.push(json[item]);
             this.linkHml.push(item);
